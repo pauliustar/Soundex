489a0b16cc91b4eee63334a4215a79c148a4b052
Paulius <paulius.taraskevicius@gmail.com>
2018-04-09 16:59:26 +0300

    Fixing Sorting Algorithm

--- include/unique_list.php
+++ include/unique_list.php
@@ -2,32 +2,28 @@
 
 foreach ($soundex as $key => $row){
     $top_index_id = 0;
-    $top_index_text = 0;
-    $counter = 0;
-    $top_soundex_id = array();
-    $top_soundex_name = array();
+    $top_value_amount = count($top_value);
 
     $max_value = array_count_values($soundex_element);
     rsort($max_value);
 
     $array_soundex[$key] = $row['soundex'];
     $array_text[$key] = $row['text'];
-    $top_soundex_name[$top_index_text] = $array_text[$key];
-    if($array_soundex[$key] === $top_value[$top_index_id]){
-        if(array_search($array_text[$key], $top_soundex_name, true) === false){
-            $top_soundex_id[$top_index_id] = $array_soundex[$key];
-            $top_soundex_name[$top_index_id] = $array_text[$key];
-            $counter++;
-            if ($counter >= $max_value[$top_index_id]) {
+
+    if($array_soundex[$key] == $top_value[$top_index_id]){
+        if ($top_index_id != $top_value_amount) {
+                $top_soundex_id = array('id' => $array_soundex[$key]);
+                $top_soundex_name = array('name' => $array_text[$key]);
                 $top_index_id++;
-                $counter = 0;
-            }
+                echo "<pre>";
+                echo "New element found = " . $top_soundex_name['name'] . " with Soundex id = " . $top_soundex_id['id']; 
+                echo "</pre>";
         }
     }
 }
 
-foreach ($top_soundex_name as $name) {
-    echo "<pre>";
-    echo $name;
-    echo "</pre>";
-}
+/*
+ if(in_array($array_soundex[$key], $top_soundex_name)){
+                echo "Element is already in top array";
+            }
+*/
